cf77ae50ae3db919baa7a0f05f1afa241551f736
scroobius-pip <sim04ful@gmail.com>
2020-02-02 18:54:08 +0100

    fixed iamges

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,18 +9,18 @@ const MetaTags = ()=>{
 Protect yourself from corona virus with these safety tips."/>
 
 <meta property="og:type" content="website"/>
-<meta property="og:url" content="https://metatags.io/"/>
+<meta property="og:url" content="https://coronawatch.now.sh/"/>
 <meta property="og:title" content="CoronaWatch - Corona virus live updates"/>
 <meta property="og:description" content="Learn more about the corona virus.
 Protect yourself from corona virus with these safety tips."/>
-<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+<meta property="og:image" content="https://i.imgur.com/aTUFPga.png"/>
 
 <meta property="twitter:card" content="summary_large_image"/>
-<meta property="twitter:url" content="https://metatags.io/"/>
+<meta property="twitter:url" content="https://coronawatch.now.sh/"/>
 <meta property="twitter:title" content="CoronaWatch - Corona virus live updates"/>
 <meta property="twitter:description" content={`Learn more about the corona virus.
 Protect yourself from corona virus with these safety tips.`}/>
-<meta property="twitter:image" content="https://Metatags.io/assets/Meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+<meta property="twitter:image" content="https://i.imgur.com/aTUFPga.png"/>
         </>
     )
 }

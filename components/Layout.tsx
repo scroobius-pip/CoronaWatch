1c7ce962fb1adeafd925ba9e57f3e7d5e6e36b29
AVATAH <emmaogah97@gmail.com>
2020-02-04 23:52:28 +0100

    more pull changes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,11 +4,7 @@ import { colors } from '../styles'
 export default ({ children }) => {
     return <div style={{ width: '100%', maxWidth: 800, margin: 'auto' }}>
         <h1 style={{ textAlign: 'center' }}>CoronaWatch:  2019-nCoV </h1>
-<<<<<<< HEAD
-        <h4 style={{ textAlign: 'center', opacity: 0.5 }}>Last updated: February 4, 2020, 22:45 GMT</h4>
-=======
         <h4 style={{ textAlign: 'center', opacity: 0.5 }}>Last updated: February 4, 2020, 22:17 GMT</h4>
->>>>>>> 7fff1a1b3b05974fd9744962910156c241b7863c
         {children}
         <style jsx global>
             {`

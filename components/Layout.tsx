fa9ba688d310515cf61a32c21a6cd94c8c193c30
AVATAH <emmaogah97@gmail.com>
2020-02-13 10:20:53 +0100

    Updated date, cases and new virus name

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,8 +3,8 @@ import { colors } from '../styles'
 
 export default ({ children }) => {
     return <div style={{ width: '100%', maxWidth: 800, margin: 'auto' }}>
-        <h1 style={{ textAlign: 'center' }}>CoronaWatch:  2019-nCoV </h1>
-        <h4 style={{ textAlign: 'center', opacity: 0.5 }}>Last updated: February 12, 2020, 07:40 GMT</h4>
+        <h1 style={{ textAlign: 'center' }}>CoronaWatch:  COVID-19 </h1>
+        <h4 style={{ textAlign: 'center', opacity: 0.5 }}>Last updated: February 13, 2020, 09:15 GMT</h4>
         {children}
         <style jsx global>
             {`

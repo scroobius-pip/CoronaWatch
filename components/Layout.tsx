f5fb3ec9635dab81737a2f8a7fdfd1951399cd89
scroobius-pip <sim04ful@gmail.com>
2020-02-14 09:49:04 +0100

    updated title  COVID-19

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@ import { colors } from '../styles'
 
 export default ({ children }) => {
     return <div style={{ width: '100%', maxWidth: 800, margin: 'auto' }}>
-        <h1 style={{ textAlign: 'center' }}>CoronaWatch:  SARS-CoV-2 </h1>
+        <h1 style={{ textAlign: 'center' }}>CoronaWatch: COVID-19 </h1>
         <h4 style={{ textAlign: 'center', opacity: 0.5 }}>Last updated: February 13, 2020, 09:25 GMT</h4>
         {children}
         <style jsx global>
